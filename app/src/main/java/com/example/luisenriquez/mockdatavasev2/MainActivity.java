4a5cc0642a11553c89db0bd1583e6ce04732395c
luis1357 <luis.enriquez1357@gmail.com>
2018-10-29 11:24:52 -0400

    mockcollar

--- app/src/main/java/com/example/luisenriquez/mockdatavasev2/MainActivity.java
+++ app/src/main/java/com/example/luisenriquez/mockdatavasev2/MainActivity.java
@@ -1,13 +1,22 @@
 package com.example.luisenriquez.mockdatavasev2;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.SyncStateContract;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.support.v4.app.FragmentActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements MainPage.OnFragmentInteractionListener,
+        Categories.OnFragmentInteractionListener
+{
 
     private TextView mTextMessage;
 
@@ -16,12 +25,19 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction ft = fragmentManager.beginTransaction();
+
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+
+                    ft.replace(R.id.fragment, new MainPage())
+                    .commit();
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                    ft.replace(R.id.fragment, new Categories())
+                            .commit();
                     return true;
                 case R.id.navigation_notifications:
                     mTextMessage.setText(R.string.title_notifications);
@@ -41,4 +57,8 @@ public class MainActivity extends AppCompatActivity {
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
-}
+            @Override
+            public void onFragmentInteraction(Uri uri) {
+
+            }
+        }
